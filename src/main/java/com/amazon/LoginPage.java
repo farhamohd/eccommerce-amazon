25e2faa860167ee1e57fe41f11a5947c4516fa07
farhamohd <farhamohd.ali@gmail.com>
2021-09-12 15:55:05 -0400

    Initial commit- amazon ecommerce selenium handson

---
+++ src/main/java/com/amazon/LoginPage.java
@@ -0,0 +1,11 @@
+package com.amazon;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+    public static void main(String[] args) {
+        WebDriver webDriver = new ChromeDriver();
+
+    }
+}
